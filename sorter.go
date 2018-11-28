5fc366701a1b94e4e396c4a63af1ad4709fd375a
alobaton <alobaton@innova4j.com>
2018-11-28 14:48:59 -0500

    Define Sort interface

---
+++ sorter.go
@@ -0,0 +1,15 @@
+package database
+
+// Sorter should be implemented by any generic SQL order we can apply to collections.
+type Sorter interface {
+	// SQL returns the portion of the code that will be merged to the query.
+	SQL() string
+}
+
+type sqlSorter struct {
+	sql string
+}
+
+func (sorter *sqlSorter) SQL() string {
+	return sorter.sql
+}
